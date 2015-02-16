67b9045267124dd0893b23f7abc027e53487655c
Alex Penson <pensona@eos.cbio.private>
2015-02-16 11:37:24 -0500

    XD fasd dir

--- plugins/fasd/fasd.plugin.zsh
+++ plugins/fasd/fasd.plugin.zsh
@@ -9,3 +9,6 @@ if [ $commands[fasd] ]; then # check if fasd is installed
   alias o='a -e open'
 fi
 
+bindkey '^X^A' fasd-complete    # C-x C-a to do fasd-complete (fils and directories)
+bindkey '^X^F' fasd-complete-f  # C-x C-f to do fasd-complete-f (only files)
+bindkey '^X^D' fasd-complete-d  # C-x C-d to do fasd-complete-d (only directories)
