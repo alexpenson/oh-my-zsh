7052f8cd26b56f47d3b42842c6f0e093b30c3fbc
Alex Penson <pensona@eos.cbio.private>
2015-02-16 11:26:11 -0500

    ll and lt

--- plugins/common-aliases/common-aliases.plugin.zsh
+++ plugins/common-aliases/common-aliases.plugin.zsh
@@ -6,8 +6,8 @@
 alias l='ls -lFh'     #size,show type,human readable
 alias la='ls -lAFh'   #long list,show almost all,show type,human readable
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
-alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
+alias lt='ls -ltrFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -hl'      #long list
 alias ldot='ls -ld .*'
 alias lS='ls -1FSsh'
 alias lart='ls -1Fcart'
